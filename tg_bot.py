0cf51ab4c8c20ad2ac8bb4d5ba0986876905d6e5
Romigo97 <roma.kolyada939@gmail.com>
2025-04-03 20:40:45 +0300

    Update tg_bot.py

--- tg_bot.py
+++ tg_bot.py
@@ -1,15 +1,48 @@
 import os
 import requests
+from time import sleep
 from dotenv import load_dotenv
 from telegram import Bot
+import logging
+from logging.handlers import RotatingFileHandler
+
+
+class TelegramLogsHandler(logging.Handler):
+    def __init__(self, tg_bot, chat_id):
+        super().__init__()
+        self.chat_id = chat_id
+        self.tg_bot = tg_bot
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        self.tg_bot.send_message(chat_id=self.chat_id, text=log_entry)
 
 
 def main():
     load_dotenv()
-    devman_token = os.getenv('DEVMAN_TOKEN')
-    tg_token = os.getenv('TG_TOKEN')
-    chat_id=os.getenv('TG_CHAT_ID')
+    devman_token = os.environ['DEVMAN_TOKEN']
+    tg_token = os.environ['TG_TOKEN']
+    chat_id=os.environ['TG_CHAT_ID']
     bot = Bot(token=tg_token)
+
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.WARNING)
+
+    rotating_handler = RotatingFileHandler(
+        'app.log',
+        maxBytes=20000,
+        backupCount=2
+    )
+    rotating_handler.setFormatter(logging.Formatter('%(process)d %(levelname)s %(message)s'))
+    logger.addHandler(rotating_handler)
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(logging.Formatter("%(process)d %(levelname)s %(message)s"))
+    logger.addHandler(stream_handler)
+
+    telegram_handler = TelegramLogsHandler(bot, chat_id)
+    telegram_handler.setFormatter(logging.Formatter("%(process)d %(levelname)s %(message)s"))
+    logger.addHandler(telegram_handler)
 
     url = 'https://dvmn.org/api/long_polling/'
     headers = {
@@ -28,7 +61,6 @@ def main():
             response.raise_for_status()
 
             devman_response = response.json()
-            print(devman_response)
 
             if devman_response['status'] == 'found':
                 timestamp = devman_response['last_attempt_timestamp']
@@ -45,13 +77,16 @@ def main():
                 lesson_url = devman_response['new_attempts'][0]['lesson_url']
                 text = f'Проверена работа "{lesson_title}".\n \nПреподавателю все понравилось, можно приступать к следующему уроку! .\n \nСсылка на урок: {lesson_url}'
                 bot.send_message(chat_id=chat_id, text=text)
-
+        except requests.exceptions.ReadTimeout:
+            continue
         except requests.exceptions.Timeout:
-            print("Сервер не отвечает. Повторяем запрос...")
+            logger.error("Сервер не отвечает. Повторяем запрос...")
         except requests.exceptions.ConnectionError:
-            print('Проблема с подключением...')
+            logger.error('Проблема с подключением...')
+            sleep(60)
         except requests.exceptions.RequestException as e:
-            print('Произошла ошибка:', e)
+            logger.exception('Произошла ошибка:', e)
+            break
 
 
 if __name__ == '__main__':
